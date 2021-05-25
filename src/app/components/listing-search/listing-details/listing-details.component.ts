1529b9ced0d371c42356d4b48b2cfc2fa8573101
Tu Nguyen <tunguyen@Tus-MacBook-Pro.local>
2021-05-24 23:28:10 -0400

    fix(listing-details): add carousel for property images

--- src/app/components/listing-search/listing-details/listing-details.component.ts
+++ src/app/components/listing-search/listing-details/listing-details.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 import { Listing } from '../listing-search.data';
 import { ListingSearchService } from '../listing-search.service';
 
@@ -8,21 +10,40 @@ import { ListingSearchService } from '../listing-search.service';
     templateUrl: 'listing-details.component.html'
 })
 
-export class ListingDetailsComponent implements OnInit, OnChanges {
+export class ListingDetailsComponent implements OnChanges {
     @Input() listingId: string = '';
     listing: Listing = {} as Listing;
 
-    constructor(private listingSearchService: ListingSearchService) {
-    }
-
-    ngOnInit() {
+    carouselInterval = 0;
+    carousel!: NgbCarousel;
+    @ViewChild('carousel', { static: false }) set content(content: NgbCarousel) {
+        if(content) { // initially setter gets called with undefined
+            this.carousel = content;
+        }
+     }
+    constructor(
+        private listingSearchService: ListingSearchService,
+        private httpClient: HttpClient,
+        private sanitizer: DomSanitizer) {
     }
 
     async ngOnChanges() {
         this.listing = await this.listingSearchService.getListingById(this.listingId);
 
-        this.listing.imageSources?.forEach(src => {
-            //TODO: get the images from Firebase
-        });
+        // TODO: replace the code below and actually retrieve images from imageSources
+        // property
+        this.listing.imageSources = [];
+        for (let i = 0; i < 10; i++) {
+            this.httpClient.get('https://picsum.photos/400/200', { responseType: 'blob' }).subscribe(response => {
+                const blob = new Blob([response], { type: 'application/image' });
+                const unsafeImg = URL.createObjectURL(blob);
+                const imageUrl = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+                this.listing.imageSources!.push(imageUrl as string);
+            });
+        }
+    }
+
+    cycleToSlide(slideId: number) {
+        this.carousel.select("ngb-slide-" + String(slideId));
     }
 }
