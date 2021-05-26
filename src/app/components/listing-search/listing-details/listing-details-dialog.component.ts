980c3c222dc7d46be65ebfed02b534492e4d4d17
Tu Nguyen <tunguyen@Tus-MacBook-Pro.local>
2021-05-26 19:14:16 -0400

    fix(listing-details-dialog): remove close button, too complicated

--- src/app/components/listing-search/listing-details/listing-details-dialog.component.ts
+++ src/app/components/listing-search/listing-details/listing-details-dialog.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-listing-details-dialog',
     templateUrl: 'listing-details-dialog.component.html'
 })
 
-export class ListingDetailsDialogComponent implements OnInit {
-    constructor() { }
+export class ListingDetailsDialogComponent {
+    constructor(public dialogRef: MatDialogRef<ListingDetailsDialogComponent>) { }
 
-    ngOnInit() { }
+    closeDialog() {
+        debugger;
+        this.dialogRef.close();
+    }
 }
