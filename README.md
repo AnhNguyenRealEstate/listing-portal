bc5a3dc7c6ff3334ba3c9b9121ae6ac2c88e2c1f
Tu Nguyen <nguyentrungtu1996@gmail.com>
2022-02-15 15:42:05 +0700

    feat(listing-edit spec): add test for listing edit service, add testing instructions for README

--- README.md
+++ README.md
@@ -6,22 +6,29 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Setting up dev/testing environment locally
+
+### Firebase Emulators
+This project uses Firebase for data storage, and so devving against this DB should be done through local DB emulators.
+
+#### 1. Download Firebase Emulator Suite and install all emulators
+#### 2. Start emulators at project root
+Run `npm run start-offlinedb` to start all emulators with ports specified in firebase.json
+It is possible for emulators to crash during the dev process, in which case run `npm run clean-offlinedb` to clean up 
+artefacts and prevent memory leaks
+
+#### 3. Add app-data collection and listing-data doc in Firestore
+Browse the Firebase Emulator page (default is localhost:4000), add app-data as a new collection, add listing-data as a new doc. In listing-data, add 2 new arrays: locations and property-types.
+#### 4. Run ng serve at project root and begin devving.
 
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+Service tests (**.service.spec.ts) are all integration tests that involve multiple services. These have to be ran one by 
+one, as they are async tests that may overlap. Run `ng test --include='**/[folder-name]/**.service.spec.ts'` to run
+individual tests.
---
+++ src/app/components/listing-edit/listing-edit.service.spec.ts
@@ -0,0 +1,134 @@
+import { TestBed } from "@angular/core/testing";
+import { collection, connectFirestoreEmulator, doc, Firestore, getDoc, getDocs, getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { connectStorageEmulator, FirebaseStorage, getStorage, provideStorage } from "@angular/fire/storage";
+import { MetadataService } from "src/app/shared/metadata.service";
+import { NgxImageCompressService } from "ngx-image-compress";
+import { firebaseConfig, FirestoreCollections } from "src/app/shared/globals";
+import { ListingEditService } from "../listing-edit/listing-edit.service";
+import { FirebaseApp, initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { Auth, connectAuthEmulator, getAuth, provideAuth, signInWithEmailAndPassword } from "@angular/fire/auth";
+import { Listing, ListingImageFile } from "../listing-search/listing-search.data";
+import { ListingUploadService } from "../listing-upload/listing-upload.service";
+
+
+describe('Listing Upload Service', () => {
+    let firebaseApp: FirebaseApp;
+    let firestore: Firestore;
+    let storage: FirebaseStorage;
+    let auth: Auth;
+
+    let listingUpload: ListingUploadService;
+    let listingEdit: ListingEditService;
+    let metadata: MetadataService;
+    let imgCompress: NgxImageCompressService;
+
+    beforeEach(async () => {
+        TestBed.configureTestingModule({
+            imports: [
+                provideFirebaseApp(() => {
+                    firebaseApp = initializeApp(firebaseConfig);
+                    return firebaseApp;
+                }),
+                provideFirestore(() => {
+                    firestore = getFirestore();
+                    connectFirestoreEmulator(firestore, 'localhost', 8080);
+                    return firestore;
+                }),
+                provideStorage(() => {
+                    storage = getStorage();
+                    connectStorageEmulator(storage, 'localhost', 9199);
+                    return storage;
+                }),
+                provideAuth(() => {
+                    auth = getAuth();
+                    connectAuthEmulator(auth, 'http://localhost:9099');
+                    return auth;
+                })],
+            providers: [
+                MetadataService,
+                NgxImageCompressService
+            ]
+        });
+
+        metadata = TestBed.inject(MetadataService);
+        imgCompress = TestBed.inject(NgxImageCompressService);
+
+        listingUpload = new ListingUploadService(firestore, storage, metadata, imgCompress);
+        listingEdit = new ListingEditService(firestore, storage);
+
+        await signInWithEmailAndPassword(auth, 'test@test.test', 'test1234!')
+    });
+
+    afterEach(async () => {
+        await auth.signOut();
+    });
+
+    /**
+     * Create a dummy listing and upload to Firestore
+     * Storage functionality is not tested as Storage emulator does not work with uploadBytes()...yet
+     */
+    it('should be able to upload a new listing, archive, unarchive and then delete it', async () => {
+        async function getFile(index: number) {
+            const response = await fetch(`test-assets/listing-upload-test-files/${index}.jpg`);
+            const blob = await response.blob();
+            return new File([blob], `${index}.jpg`, { type: blob.type });
+        };
+
+        function generateRandomListing(): Listing {
+            return {
+                propertyType: 'Apartment',
+                location: 'Hung Gia 1',
+                bedrooms: Math.floor(Math.random() * 100),
+                bathrooms: Math.floor(Math.random() * 100),
+                address: 'This is a` test',
+                description: 'This is a test',
+                archived: true,
+                purpose: 'For Rent'
+            }
+        }
+
+        // Publish the listing
+        const listing = generateRandomListing();
+
+        const numOfImages = 10;
+        const imageFilesWithRandomImgs: ListingImageFile[] = [];
+        for (let i = 0; i < numOfImages; i++) {
+            const result = await getFile(i);
+            imageFilesWithRandomImgs.push({ raw: result } as ListingImageFile);
+        }
+
+        let imageHasSize = false;
+        imageFilesWithRandomImgs.forEach(img => {
+            imageHasSize = imageHasSize || !!(img?.raw?.size);
+        })
+        expect(imageHasSize).toBe(true);
+
+        let testDocId = await listingUpload.publishListing(listing, imageFilesWithRandomImgs);
+        expect(testDocId).toBeTruthy();
+
+        // Archive the listing
+        listingEdit.archiveListing(testDocId);
+        const archivedListing =
+            (await getDoc(doc(collection(firestore, FirestoreCollections.listings), testDocId)))
+                .data() as Listing;
+        expect(archivedListing.archived).toBe(true);
+
+        // Unarchive the listing
+        listingEdit.unarchiveListing(testDocId);
+        const unarchivedListing =
+            (await getDoc(doc(collection(firestore, FirestoreCollections.listings), testDocId)))
+                .data() as Listing;
+        expect(unarchivedListing.archived).toBe(false);
+
+        // Delete the listing
+        let response = await getDoc(doc(collection(firestore, FirestoreCollections.listings), testDocId));
+        expect(response.exists()).toBe(true);
+        const listingFromServer = response.data() as Listing;
+        const id = response.id;
+        await listingEdit.deleteListing(listingFromServer, id);
+
+
+        const docStillExistsOnDb = !!(await getDocs(collection(firestore, FirestoreCollections.listings))).docs.filter(doc => doc.id === id).length;
+        expect(docStillExistsOnDb).toBe(false);
+    });
+})
