fb324ba1121326052606fdbab07afe6edaab3f36
Tu Nguyen <nguyentrungtu1996@gmail.com>
2022-03-20 16:28:58 +0700

    feat(employee-reporting): init employee reporting module

---
+++ src/app/employee-reporting/employee-reporting.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'employee-reporting',
+    templateUrl: 'employee-reporting.component.html'
+})
+
+export class EmployeeReportingComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
