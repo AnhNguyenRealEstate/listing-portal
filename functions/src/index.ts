a4b13a703af7c880d28e25c03d38924b045b4189
Tu Nguyen <nguyentrungtu1996@gmail.com>
2022-03-31 16:40:25 +0700

    feat(post process listing deletion): add function to remove location from metadata if no listings have reference to it

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ import * as firebase from "firebase-admin";
 firebase.initializeApp();
 
 /**
+ * After a listing's creation
  * Update id an creation date for a listing upon upload
  */
 exports.postProcessListingCreation = functions.region('asia-southeast1').firestore
@@ -32,6 +33,50 @@ exports.postProcessListingUpdate = functions.region('asia-southeast1').firestore
     const listingLocation = listingData['location'];
     updateLocationsMetadata(listingLocation);
   });
+
+/**
+* After a listing's update
+* Update app-data/listing-data/locations
+*/
+exports.postProcessListingUpdate = functions.region('asia-southeast1').firestore
+  .document('listings/{documentId}')
+  .onUpdate(async (change) => {
+    const listingData = change.after.data();
+    const listingLocation = listingData['location'];
+    updateLocationsMetadata(listingLocation);
+  });
+
+/**
+ * After a listing's removal
+ * Remove the listing's location if it is the last item to contain such location
+ */
+exports.postProcessListingDelete = functions.region('asia-southeast1').firestore
+  .document('listings/{documentId}')
+  .onDelete(async (snap) => {
+    const locationOfDeletedListing = snap.data()['location'] as string;
+    const listingsColSnap = await firebase
+      .firestore()
+      .collection('listings')
+      .where("location", "==", locationOfDeletedListing)
+      .get();
+
+    if (listingsColSnap.docs.length) {
+      return;
+    }
+
+    const listingMetadataSnap = await firebase.firestore().doc('app-data/listing-data').get();
+    const listingMetadata = listingMetadataSnap.data();
+    if (!listingMetadata) {
+      return;
+    }
+
+    const locationsMetadata = listingMetadata['locations'] as string[];
+    const newLocMetadata = locationsMetadata.filter(loc => loc !== locationOfDeletedListing);
+    listingMetadataSnap.ref.update({
+      "locations": newLocMetadata
+    });
+  });
+
 
 async function updateLocationsMetadata(location: string) {
   const listingMetadataSnap = await firebase.firestore().doc('app-data/listing-data').get();
