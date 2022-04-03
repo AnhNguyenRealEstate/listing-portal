99705f0c637d883ce6e90b65ad9e42de73605e11
Tu Nguyen <nguyentrungtu1996@gmail.com>
2022-04-03 18:36:47 +0700

    feat(functions): remove host function until further development

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 import * as functions from "firebase-functions";
 import * as firebase from "firebase-admin";
 import * as currency from 'currency.js'
-
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
 
 firebase.initializeApp();
 
@@ -82,68 +81,68 @@ exports.postProcessListingDelete = functions.region('asia-southeast1').firestore
   });
 
 
-exports.host = functions.region('asia-southeast1').https.onRequest(async (req, res) => {
-  const isListingDetailsPage = req.url.indexOf('/listings/details/') == -1;
-  const indexHTMLpath = String(path.join(__dirname, 'hosting', 'index.html'));
-  let indexHTML = fs.readFileSync(indexHTMLpath).toString();
-  const defaultDesc = 'Real estate services in District 7, Ho Chi Minh City';
-  const defaultTitle = 'Anh Nguyen Real Estate';
-  const defaultLogo = '/assets/images/logo.png';
-  const defaultUrl = 'https://anhnguyenre.com';
+// exports.host = functions.region('asia-southeast1').https.onRequest(async (req, res) => {
+//   const isListingDetailsPage = req.url.indexOf('/listings/details/') == -1;
+//   const indexHTMLpath = path.join(__dirname, 'hosting', 'index.html');
+//   let indexHTML = fs.readFileSync(indexHTMLpath).toString();
+//   const defaultDesc = 'Real estate services in District 7, Ho Chi Minh City';
+//   const defaultTitle = 'Anh Nguyen Real Estate';
+//   const defaultLogo = '/assets/images/logo.png';
+//   const defaultUrl = 'https://anhnguyenre.com';
 
-  const getOpenGraph = async (isListingDetailsPage: boolean) => {
-    let og = `<meta property="og:type" content="website">`;
+//   const getOpenGraph = async (isListingDetailsPage: boolean) => {
+//     let og = `<meta property="og:type" content="website">`;
 
-    if (!isListingDetailsPage) {
-      og += `<meta property="og:title" content="${defaultTitle}" />`;
-      og += `<meta property="og:description" content="${defaultDesc}" />`;
-      og += `<meta property="og:image" content="${defaultLogo}" />
-            <meta property="og:image:type" content="image/*">
-            <meta property="og:image:width" content="1200">
-            <meta property="og:image:height" content="600">`;
-      og += `<meta property="og:url" content="${defaultUrl}" />`;
-      return og;
-    }
+//     if (!isListingDetailsPage) {
+//       og += `<meta property="og:title" content="${defaultTitle}" />`;
+//       og += `<meta property="og:description" content="${defaultDesc}" />`;
+//       og += `<meta property="og:image" content="${defaultLogo}" />
+//             <meta property="og:image:type" content="image/*">
+//             <meta property="og:image:width" content="1200">
+//             <meta property="og:image:height" content="600">`;
+//       og += `<meta property="og:url" content="${defaultUrl}" />`;
+//       return og;
+//     }
 
-    const urlComponents = req.url.split('/');
-    const listingID = urlComponents[urlComponents.length - 1];
-    const docRef = firebase.firestore().collection('listings').doc(listingID);
-    const listing = (await docRef.get()).data() as any;
+//     const urlComponents = req.url.split('/');
+//     const listingID = urlComponents[urlComponents.length - 1];
+//     const docRef = firebase.firestore().collection('listings').doc(listingID);
+//     const listing = (await docRef.get()).data() as any;
 
-    const VND = (value: number) => currency(value, { symbol: "đ", separator: ",", precision: 2 });
-    const oneBillion = 1000000000;
-    const USD = (value: number) => currency(value, { symbol: "$", separator: "," });
+//     const VND = (value: number) => currency(value, { symbol: "đ", separator: ",", precision: 2 });
+//     const oneBillion = 1000000000;
+//     const USD = (value: number) => currency(value, { symbol: "$", separator: "," });
 
-    let priceText = '';
-    if (listing.currency === 'VND') {
-      priceText = VND(listing['price']).divide(oneBillion).format();
-    } else if (listing.currency === 'USD') {
-      priceText = USD(listing['price']).format();
-    }
-    const ogTitle = `${listing['location']} - ${priceText}`;
+//     let priceText = '';
+//     if (listing.currency === 'VND') {
+//       priceText = VND(listing['price']).divide(oneBillion).format();
+//     } else if (listing.currency === 'USD') {
+//       priceText = USD(listing['price']).format();
+//     }
+//     const ogTitle = `${listing['location']} - ${priceText}`;
 
-    const ogDesc = `${listing['address']}`;
-    const ogUrl = req.url;
+//     const ogDesc = `${listing['address']}`;
+//     const ogUrl = req.url;
 
-    const coverImageUrl = firebase.storage().bucket().file(listing['coverImagePath']).publicUrl();
-    const ogImage = `${coverImageUrl}`;
+//     const coverImageUrl = firebase.storage().bucket().file(listing['coverImagePath']).publicUrl();
+//     const ogImage = `${coverImageUrl}`;
 
-    og += `< meta property = "og:title" content = "${ogTitle}}" /> `;
-    og += `< meta property = "og:description" content = "${ogDesc || defaultDesc}" /> `;
-    og += `< meta property = "og:image" content = "${ogImage || defaultLogo}" />
-          <meta property="og:image:type" content = "image/*" >
-          <meta property="og:image:width" content = "1200" >
-          <meta property="og:image:height" content = "600" > `;
+//     og += `< meta property = "og:title" content = "${ogTitle}}" /> `;
+//     og += `< meta property = "og:description" content = "${ogDesc || defaultDesc}" /> `;
+//     og += `< meta property = "og:image" content = "${ogImage || defaultLogo}" />
+//           <meta property="og:image:type" content = "image/*" >
+//           <meta property="og:image:width" content = "1200" >
+//           <meta property="og:image:height" content = "600" > `;
 
-    og += `< meta property = "og:url" content = "${ogUrl || defaultUrl}" /> `;
-    return og;
-  };
+//     og += `< meta property = "og:url" content = "${ogUrl || defaultUrl}" /> `;
+//     return og;
+//   };
 
-  const ogPlaceholder = '<meta name="functions-insert-dynamic-og">';
-  const ogReplacement = await getOpenGraph(isListingDetailsPage);
-  indexHTML = indexHTML.replace(ogPlaceholder, ogReplacement);
-  res.status(200).send(indexHTML);
-});
+//   const ogPlaceholder = '<meta name="functions-insert-dynamic-og">';
+//   const ogReplacement = await getOpenGraph(isListingDetailsPage);
+//   indexHTML = indexHTML.replace(ogPlaceholder, ogReplacement);
+//   res.status(200).send(indexHTML);
+// });
 
 async function updateLocationsMetadata(location: string) {
   const listingMetadataSnap = await firebase.firestore().doc('app-data/listing-data').get();
