40b51aaea04568ac6c23692dd52e7eca62dc7f53
Tu Nguyen <nguyentrungtu1996@gmail.com>
2022-04-07 18:07:41 +0700

    feat(listing-card): add btn to copy link to listing

--- src/app/listing-card/listing-card.component.ts
+++ src/app/listing-card/listing-card.component.ts
@@ -1,3 +1,4 @@
+import { Clipboard } from '@angular/cdk/clipboard';
 import { Component, Input, OnInit } from '@angular/core';
 import { getDownloadURL, ref, Storage } from '@angular/fire/storage';
 import { Listing } from '../listing-search/listing-search.data';
@@ -13,10 +14,24 @@ export class ListingCardComponent implements OnInit {
     @Input() listing!: Listing;
 
     coverImageUrl: string = '';
+    shareListingBtnTitle: string = '';
 
-    constructor(private storage: Storage) { }
+    constructor(
+        private storage: Storage,
+        private clipboard: Clipboard) { }
 
     async ngOnInit() {
-       this.coverImageUrl = await getDownloadURL(ref(this.storage, this.listing.coverImagePath));
+        getDownloadURL(ref(this.storage, this.listing.coverImagePath)).then(url => {
+            this.coverImageUrl = url;
+        });
+    }
+
+    getShareableLink(e: Event) {
+        e.stopPropagation();
+
+        if (!this.listing) {
+            return;
+        }
+        this.clipboard.copy(`${window.location.host}/listings/details/${this.listing.id}`);
     }
 }
--- src/app/listing-card/listing-card.module.ts
+++ src/app/listing-card/listing-card.module.ts
@@ -4,6 +4,8 @@ import { ListingCardComponent } from './listing-card.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { TranslateModule } from '@ngx-translate/core';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ListingCardComponent],
@@ -11,9 +13,11 @@ import { TranslateModule } from '@ngx-translate/core';
     CommonModule,
     MatIconModule,
     MatCardModule,
+    MatButtonModule,
     TranslateModule.forChild({
       extend: true
-    })
+    }),
+    ClipboardModule
   ],
   exports: [ListingCardComponent]
 })
