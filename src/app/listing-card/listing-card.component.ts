a6a5998f6c7b6dd754d4e3d930ba2d81dae35be9
Tu Nguyen <nguyentrungtu1996@gmail.com>
2022-04-09 11:53:29 +0700

    feat(listing-card): add snackbar for show link copied message

--- src/app/listing-card/listing-card.component.ts
+++ src/app/listing-card/listing-card.component.ts
@@ -1,8 +1,10 @@
 import { Clipboard } from '@angular/cdk/clipboard';
 import { Component, Input, OnInit } from '@angular/core';
 import { getDownloadURL, ref, Storage } from '@angular/fire/storage';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { lastValueFrom } from 'rxjs';
 import { Listing } from '../listing-search/listing-search.data';
 
 @Component({
@@ -17,26 +19,37 @@ export class ListingCardComponent implements OnInit {
     coverImageUrl: string = '';
     shareListingBtnTitle: string = '';
 
+    linkCopiedMsg: string = '';
+
     constructor(
         private storage: Storage,
         private clipboard: Clipboard,
         private router: Router,
+        private snackbar: MatSnackBar,
         public translate: TranslateService) { }
 
     async ngOnInit() {
         getDownloadURL(ref(this.storage, this.listing.coverImagePath)).then(url => {
             this.coverImageUrl = url;
         });
+
+        this.linkCopiedMsg = await lastValueFrom(this.translate.get('listing_card.link_copied'));
     }
 
     getShareableLink(e: Event) {
-        e.preventDefault();
         e.stopPropagation();
 
         if (!this.listing) {
             return;
         }
+
         this.clipboard.copy(`${window.location.host}/listings/details/${this.listing.id}`);
+        this.snackbar.open(
+            this.linkCopiedMsg,
+            undefined,
+            {
+                duration: 1000
+            })
     }
 
     showListing() {
